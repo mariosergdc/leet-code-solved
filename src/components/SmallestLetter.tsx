e34b56051fca611071f619ab528a7d99eb4c01f4
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-09-08 13:09:11 -0400

    Smallest Letter

---
+++ src/components/SmallestLetter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SmallestLetter = () => {
+  /**
+   * @param {character[]} letters
+   * @param {character} target
+   * @return {character}
+   */
+  var nextGreatestLetter = function (letters, target) {
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i] > target) return letters[i];
+    }
+    return letters[0];
+  };
+  return <div>SmallestLetter</div>;
+};
+
+export default SmallestLetter;
