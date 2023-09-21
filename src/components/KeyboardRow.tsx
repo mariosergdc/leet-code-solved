2758d256e8bfda56bdacfdc6429282b76c116932
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-09-20 21:05:04 -0400

    Keyboard Row ok

--- src/components/KeyboardRow.tsx
+++ src/components/KeyboardRow.tsx
@@ -6,20 +6,35 @@ const KeyboardRow = () => {
    * @return {string[]}
    */
   var findWords = function (words) {
-    let first = "qwertyuiop";
-    console.log(first);
-    let second = "asdfghjkl";
-    let third = "zxcvbnm";
-    let bfirst = true;
-    let bsecond = true;
-    let bthird = true;
+    let first = "qwertyuiopQWERTYUIOP";
+    let second = "asdfghjklASDFGHJKL";
+    let third = "zxcvbnmZXCVBNM";
+    let bfirst = 0;
+    let bsecond = 0;
+    let bthird = 0;
+    let res = [];
     for (let i = 0; i < words.length; i++) {
       for (let j = 0; j < words[i].length; j++) {
-        if (!first.includes(words[i][j])) {
-          bfirst = false;
+        if (first.includes(words[i][j])) {
+          bfirst++;
+        } else if (second.includes(words[i][j])) {
+          bsecond++;
+        } else if (third.includes(words[i][j])) {
+          bthird++;
         }
       }
+      if (
+        bfirst === words[i].length ||
+        bsecond === words[i].length ||
+        bthird === words[i].length
+      ) {
+        res.push(words[i]);
+      }
+      bfirst = 0;
+      bsecond = 0;
+      bthird = 0;
     }
+    return res;
   };
   return <div>KeyboardRow</div>;
 };
