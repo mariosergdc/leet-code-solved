0f9b9191f13b77c83dd07d4651da5bdb4742fc2b
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-10-02 15:38:13 -0400

    sqrt floor x

---
+++ src/components/Sqrt.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sqrt = () => {
+  /**
+   * @param {number} x
+   * @return {number}
+   */
+  var mySqrt = function (x) {
+    return Math.floor(Math.sqrt(x));
+  };
+  return <div>Sqrt</div>;
+};
+
+export default Sqrt;
