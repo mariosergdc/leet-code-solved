adaf386fda4638eb3b3dd82f6cce24aa8ee5eeac
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-10-09 19:25:27 -0400

    find-first-and-last-position-of-element-in-sorted-array

---
+++ src/components/Problem34.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Problem34 = () => {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  var searchRange = function (nums, target) {
+    let first = -1,
+      last = -1;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) {
+        if (first === -1) {
+          first = i;
+        }
+        last = i;
+      }
+    }
+    return [first, last];
+  };
+  return <div>Problem34</div>;
+};
+
+export default Problem34;
