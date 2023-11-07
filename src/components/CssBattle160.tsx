3377c08fdfa3c23b2165185d415d58ea49ee915a
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-11-07 12:47:10 -0500

    css battle 160

---
+++ src/components/CssBattle160.tsx
@@ -0,0 +1,27 @@
+<div class="c"> <div class="circle">
+    
+  </div> </div>
+  
+
+<style>
+  *{
+    margin:0;
+  }
+  .c{
+    width:400px;
+    height:300px;
+    background:#62306D;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+  .circle {
+    width: 120px;
+    height: 120px;
+    background: #62306D;
+    border-radius:50%;
+    border:solid #F7EC7D 40px;
+    border-top-color:#E38F66;
+    rotate:-45deg;
+  }
+</style>
